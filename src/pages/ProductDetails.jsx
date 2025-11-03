141c15967febe0991f3b6b3fefc1afbacddc90ae
Danniel Canary Quipia <dannielcanaryq@gmail.com>
2025-11-03 15:02:23 +0800

    scan me finished product

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+ import React from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { products } from '../data/products';
 import ImageSlider from '../components/ImageSlider';
